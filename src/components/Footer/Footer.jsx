490f8fbf5400630ec4c3fb9f4ee690665ac964e2
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-10-23 11:37:15 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,12 @@ import "./footer.css"
 function Footer(){
     return(
         <div className="footer-container">
-            Footer
+            <h2>You can find me </h2>
+            <div className="icons">
+                <a className="link" href="https://github.com/tezcanTunahan"><i class="fab fa-github"></i></a>
+                <a className="link" href="https://www.linkedin.com/in/tunahan-t-055244211/"><i class="fab fa-linkedin"></i></a>
+                <a className="link" href="https://www.instagram.com/tunahantezcaan/"><i class="fab fa-instagram"></i></a>
+            </div>
         </div>
     )
 }
