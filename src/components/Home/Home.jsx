b772ee3b72a0f4b072e0f073b2ba33801413501c
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-10-23 10:48:32 +0300

    projects addded

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,8 @@ import "./home.css"
 function Home(){
     return(
         <div className="home-container">
-          
+            <h1 className="home-h1">Hi, <br />I'm Tunahan, <br /> I’m full stack web developer</h1>
+            <button className="home-btn">Contact me</button>
         </div>
     )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,60 @@
 import "./projects.css"
+import residue from "../../img/residue.png"
+import blogT from "../../img/blogT.png"
+import todo from "../../img/todo.png"
+
+
 
 function Projects(){
+
+    function openResidue(){
+        window.location.replace("https://residue.netlify.app/");
+    }
+    function openBlogT(){
+        window.location.replace("https://blogt.netlify.app/");
+    } 
+    function openTodo(){
+        window.location.replace("https://todowithtimer.netlify.app/");
+    }
+
     return(
         <div className="projects-container">
-            Projects
+            <div onClick={openResidue} class="flip-card">
+            <div  class="flip-card-inner">
+                <div class="flip-card-front">
+                <img className="proje-img" alt="proje-img" src={residue} />
+                </div>
+                <div class="flip-card-back">
+                <h1>Residue</h1> 
+                <p>It's MERN stack app</p> 
+                <p>I used mapbox for this one</p>
+                </div>
+            </div>
+            </div>
+            <div onClick={openBlogT} class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                <img className="proje-img" alt="proje-img" src={blogT} />
+                </div>
+                <div class="flip-card-back">
+                <h1>BlogT</h1> 
+                <p>It's my first MERN Stack proje</p> 
+                <p>This one teach me how to use frontend and backand together</p>
+                </div>
+            </div>
+            </div>
+            <div onClick={openTodo} class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                <img className="proje-img" alt="proje-img" src={todo} />
+                </div>
+                <div class="flip-card-back">
+                <h1>Todo-Timer</h1> 
+                <p>It's my first 'react' proje</p> 
+                <p>It's pretty ugly</p>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
